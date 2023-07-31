acad4fafa27054e7e58acea62481de57583f524c
Kim Dae Hwi <121790935+marooo326@users.noreply.github.com>
2023-07-31 16:24:13 +0900

    [level 2] Title: 가장 큰 수, Time: 3.63 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        Integer[] IntegerNums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        String[] sortedNums = new String[numbers.length];
+        Arrays.sort(IntegerNums, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2){
+                String s1 = Integer.toString(o1);
+                String s2 = Integer.toString(o2);
+                if (Long.parseLong(s1+s2) > Long.parseLong(s2+s1)){
+                    return -1;
+                } else if (Long.parseLong(s1+s2) < Long.parseLong(s2+s1)){
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        
+        for (int i=0; i<numbers.length; ++i){
+            sortedNums[i] = Integer.toString(IntegerNums[i]);
+        }
+
+        if (sortedNums[0].equals("0")){
+            return "0";
+        }
+        return String.join("",sortedNums);
+    }
+}
