ed21955c4bb71a837e83d367126d6d86a3fc14be
Kim Dae Hwi <121790935+marooo326@users.noreply.github.com>
2023-08-06 23:57:16 +0900

    [level 2] Title: H-Index, Time: 0.23 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Integer[] intArray = new Integer[citations.length];
+
+        for(int i = 0; i < citations.length; i++)
+            intArray[i] = citations[i];
+
+        Arrays.sort(intArray, Collections.reverseOrder());
+
+        for(int i = 0; i < intArray.length; i++) {
+            if(intArray[i] <= i+1)
+                return i;
+        }
+
+        return intArray.length;
+    }
+}
