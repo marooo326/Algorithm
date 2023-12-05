08617615db8422d43ebd2ccbe2bdff0394c4651a
Kim Dae Hwi <121790935+marooo326@users.noreply.github.com>
2023-12-05 21:28:40 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 144 ms, Memory: 16000 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -3,45 +3,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        List<Integer> nums = new ArrayList<>();
-        Integer a = Integer.parseInt(st.nextToken());
-        Integer b = Integer.parseInt(st.nextToken());
-
-        if (a > b) {
-            Integer temp = b;
-            b = a;
-            a = temp;
-        }
-
-        int copyA = a;
-        int copyB = b;
-
-        for (int i = 2; i <= a / 2; ++i) {
-            if (a % i == 0) {
-                while (copyA % i == 0) {
-                    nums.add(i);
-                    copyA /= i;
-                }
-            }
-        }
-        if (copyA != 1) {
-            nums.add(copyA);
-        }
-
-        int gcd = 1;
-        for (Integer num : nums) {
-            if (b % num == 0) {
-                gcd *= num;
-                b /= num;
-            }
-        }
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int gcd = gcd(a, b);
+        int lcm = a * b / gcd;
         bw.write(gcd + "\n");
-        bw.write(a * b + "");
+        bw.write(lcm + "");
         bw.flush();
     }
 }
